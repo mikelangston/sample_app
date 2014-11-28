ee23e9ba07bc7089d016c51b6e72d190acc8f5b7
mikelangston <mikemac8203@gmail.com>
2014-11-28 13:58:39 -0500

    Added email validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
   def new
   	@title = "Sign Up"
   end
+
+  def create
+  	User.create(user_params)
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email)
+  end
+
 end
